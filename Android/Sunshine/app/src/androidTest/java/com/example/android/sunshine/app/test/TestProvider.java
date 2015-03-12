befb247794f4c16529f561fd68e35e5a666409b8
Damodar <itsdamslife@gmail.com>
2015-03-12 13:09:47 +0530

    added unit tests for db insertion

--- Android/Sunshine/app/src/androidTest/java/com/example/android/sunshine/app/test/TestProvider.java
+++ Android/Sunshine/app/src/androidTest/java/com/example/android/sunshine/app/test/TestProvider.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Build;
 import android.test.AndroidTestCase;
 import android.util.Log;
@@ -64,8 +65,8 @@ public class TestProvider extends AndroidTestCase {
         // Fantastic.  Now that we have a location, add some weather!
         ContentValues weatherValues = TestDb.createWeatherValues(locationRowId);
 
-        long weatherRowId = db.insert(WeatherEntry.TABLE_NAME, null, weatherValues);
-        assertTrue(weatherRowId != -1);
+        Uri insertUri = mContext.getContentResolver().insert(WeatherEntry.CONTENT_URI, weatherValues);
+//        long weatherRowId = ContentUris.parseId(insertUri); // Example of getting weather Id
 
         // A cursor is your primary interface to the query results.
         Cursor weatherCursor = mContext.getContentResolver().query(
